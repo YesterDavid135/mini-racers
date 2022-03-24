a46b24bd7d00c85c172e237de6caa299c844e3bd
cashlessxd <mnoel8393@gmail.com>
2022-03-24 11:56:21 +0100

    Changed Hard Tyre Property

--- src/backend/tyre/HardCompound.java
+++ src/backend/tyre/HardCompound.java
@@ -4,7 +4,7 @@ public class HardCompound extends Tyre {
 
     public HardCompound() {
         super(TyreType.HARD);
-        setCompoundInfluence(1.01);
+        setCompoundInfluence(1.02);
     }
 
     public void updateTyreCondition(double driverSkill) {
