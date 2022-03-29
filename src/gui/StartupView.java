71e8b0fb900787e5ed6b0ddd7631833eb1c487d2
yesterdavid135 <david.minder@sunrise.ch>
2022-03-29 08:27:51 +0200

    Add StartupView GUI

---
+++ src/gui/StartupView.java
@@ -0,0 +1,112 @@
+/*
+ * Created by JFormDesigner on Tue Mar 29 08:24:18 CEST 2022
+ */
+
+package gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author unknown
+ */
+public class StartupView extends JFrame {
+    public StartupView() {
+        initComponents();
+    }
+
+    private void ok(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void cancel(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        label1 = new JLabel();
+        playerName = new JTextField();
+        playerNumber = new JSpinner();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+        cancelButton = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new MigLayout(
+                    "insets dialog,hidemode 3",
+                    // columns
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]",
+                    // rows
+                    "[]" +
+                    "[]" +
+                    "[]" +
+                    "[]"));
+
+                //---- label1 ----
+                label1.setText("Mini Racers");
+                contentPanel.add(label1, "cell 5 0");
+                contentPanel.add(playerName, "cell 2 3");
+                contentPanel.add(playerNumber, "cell 5 3");
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setLayout(new MigLayout(
+                    "insets dialog,alignx right",
+                    // columns
+                    "[button,fill]" +
+                    "[button,fill]" +
+                    "[button,fill]",
+                    // rows
+                    null));
+
+                //---- okButton ----
+                okButton.setText("Run");
+                okButton.addActionListener(e -> ok(e));
+                buttonBar.add(okButton, "cell 0 0");
+
+                //---- cancelButton ----
+                cancelButton.setText("Stop");
+                cancelButton.addActionListener(e -> cancel(e));
+                buttonBar.add(cancelButton, "cell 1 0");
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JLabel label1;
+    private JTextField playerName;
+    private JSpinner playerNumber;
+    private JPanel buttonBar;
+    private JButton okButton;
+    private JButton cancelButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
